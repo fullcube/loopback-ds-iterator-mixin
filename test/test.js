6a1c154636867f4a966fd2a86b794c27150b7198
Tom Kirkpatrick <tom@systemseed.com>
2015-11-01 18:41:34 +0100

    eslint

--- test/test.js
+++ test/test.js
@@ -1,21 +1,16 @@
-/* jshint mocha: true */
+const path = require('path');
+const loopback = require('loopback');
+const lt = require('loopback-testing');
+const chai = require('chai');
+const expect = chai.expect;
 
-var debug = require('debug')('loopback-ds-iterator-mixin');
-var path = require('path');
-
-var loopback = require('loopback');
-var lt = require('loopback-testing');
-
-var chai = require('chai');
-var expect = chai.expect;
-var assert = chai.assert;
-
-var sinon = require('sinon');
 chai.use(require('sinon-chai'));
 require('mocha-sinon');
 
 // Create a new loopback app.
-var app = loopback();
+const app = loopback();
+
+import { it, beforeEach } from 'arrow-mocha';
 
 // Set up promise support for loopback in non-ES6 runtime environment.
 global.Promise = require('bluebird');
@@ -24,28 +19,27 @@ global.Promise = require('bluebird');
 require(path.join('..', 'lib'))(app);
 
 // Configure datasource
-let dbConnector = loopback.memory();
+const dbConnector = loopback.memory();
 
-describe('loopback datasource iterator mixin', function () {
-
-  beforeEach(function (done) {
-    var Item = this.Item = loopback.PersistedModel.extend('item', {
+describe('loopback datasource iterator mixin', () => {
+  beforeEach((t, done) => {
+    t.Item = loopback.PersistedModel.extend('item', {
       name: String,
       description: String,
-      status: String
+      status: String,
     }, {
       mixins: {
         Iterator: {
           batchSize: 5,
           maxQueueLength: 10,
           queueWaitInterval: 100,
-          concurrentItems: 25
-        }
-      }
+          concurrentItems: 25,
+        },
+      },
     });
 
-    Item.attachTo(dbConnector);
-    app.model(Item);
+    t.Item.attachTo(dbConnector);
+    app.model(t.Item);
 
     app.use(loopback.rest());
     app.set('legacyExplorer', false);
@@ -54,143 +48,145 @@ describe('loopback datasource iterator mixin', function () {
 
   lt.beforeEach.withApp(app);
 
-  for (var i = 1; i <= 100; i++) {
+  for (let i = 1; i <= 100; i++) {
     lt.beforeEach.givenModel('item', {
-      name: 'Item' + i,
-      description: 'This is item with id' + i,
-      status: i % 2? 'active' : 'disabled'
-    }, 'item' + i);
+      name: `Item ${ i }`,
+      description: `'This is item with id ${ i }`,
+      status: i % 2 ? 'active' : 'disabled',
+    }, `Item ${ i }`);
   }
 
-  describe('mixin', function () {
-    it('should provide an iterate method', function () {
-      expect(this.Item.iterate).to.be.a('function');
+  describe('mixin', () => {
+    it('should provide an iterate method', (t) => {
+      expect(t.Item.iterate).to.be.a('.then(() => {');
     });
-    it('should provide a forEachAsync method', function () {
-      expect(this.Item.forEachAsync).to.be.a('function');
+    it('should provide a forEachAsync method', (t) => {
+      expect(t.Item.forEachAsync).to.be.a('function');
     });
   });
 
-  describe('Model.iterate', function () {
-    it('should return an Iterator instance', function () {
-      var iterator = this.Item.iterate();
+  describe('Model.iterate', () => {
+    it('should return an Iterator instance', (t) => {
+      const iterator = t.Item.iterate();
+
       expect(iterator).respondTo('next');
       expect(iterator).respondTo('forEachAsync');
     });
-    it('should allow overriding the default batchSize', function () {
-      var iterator = this.Item.iterate({}, {batchSize: 5});
+    it('should allow overriding the default batchSize', (t) => {
+      const iterator = t.Item.iterate({}, { batchSize: 5 });
+
       expect(iterator.batchSize).to.equal(5);
     });
   });
 
 
-  describe('Model.forEachAsync', function () {
-    it('should process all items sequentially when calling forEachAsync()', function (done) {
-      var count = 1;
-      var fn = function(task, callback) {
-        setTimeout(function() {
-          expect(task.item.name).to.equal('Item'+count);
+  describe('Model.forEachAsync', () => {
+    it('should process all items sequentially when calling forEachAsync()', (t, done) => {
+      let count = 1;
+
+      t.Item.forEachAsync({}, (task, callback) => {
+        setTimeout(() => {
+          expect(task.item.name).to.equal(`Item ${ count }`);
           count++;
           callback();
         }, 10);
-      }
-      this.Item.forEachAsync({}, fn, {})
-        .then(function () {
-          done();
-        })
+      }, {})
+        .then(done)
         .catch(done);
     });
   });
 
 
-  describe('IteratorCls', function () {
-    it('should initialize an iterator with pager and count', function (done) {
-      var iterator = this.Item.iterate();
+  describe('IteratorCls', () => {
+    it('should initialize an iterator with pager and count', (t, done) => {
+      const iterator = t.Item.iterate();
+
       iterator.initialize()
-        .then(function() {
+        .then(() => {
           expect(iterator.currentItem).to.equal(0);
           expect(iterator.itemsTotal).to.equal(100);
           expect(iterator.currentItems).to.be.an('array');
           expect(iterator.currentItems).to.have.length(0);
-          done();
         })
+        .then(done)
         .catch(done);
     });
-    it('should filter items using a where query', function (done) {
-      var iterator = this.Item.iterate({where: {status: 'active'}});
+    it('should filter items using a where query', (t, done) => {
+      const iterator = t.Item.iterate({ where: { status: 'active' } });
+
       iterator.initialize()
-        .then(function() {
+        .then(() => {
           expect(iterator.itemsTotal).to.equal(50);
-          done();
         })
+        .then(done)
         .catch(done);
     });
-    it('should honor a query limit', function (done) {
-      var iterator = this.Item.iterate({limit: 2});
+    it('should honor a query limit', (t, done) => {
+      const iterator = t.Item.iterate({ limit: 2 });
+
       expect(iterator.limit).to.equal(2);
       iterator.next()
-        .then(function (item) {
+        .then((item) => {
           expect(item.name).to.equal('Item1');
           expect(iterator.itemsTotal).to.equal(2);
           return iterator.next();
         })
-        .then(function (item) {
+        .then((item) => {
           expect(item.name).to.equal('Item2');
           return iterator.next();
         })
-        .then(function (item) {
-          expect(item).to.equal(undefined);
-          done();
+        .then((item) => {
+          expect(typeof item).to.equal('undefined');
         })
+        .then(done)
         .catch(done);
     });
-    it('should honor a query skip', function (done) {
-      var iterator = this.Item.iterate({skip: 2});
+    it('should honor a query skip', (t, done) => {
+      const iterator = t.Item.iterate({ skip: 2 });
+
       iterator.next()
-        .then(function (item) {
+        .then((item) => {
           expect(item.name).to.equal('Item3');
           expect(iterator.itemsFrom).to.equal(2);
-          done();
         })
+        .then(done)
         .catch(done);
     });
-    it('should iterate when calling next()', function (done) {
-      var iterator = this.Item.iterate();
+    it('should iterate when calling next()', (t, done) => {
+      const iterator = t.Item.iterate();
+
       iterator.next()
-        .then(function (item) {
+        .then((item) => {
           expect(item.name).to.equal('Item1');
           return iterator.next();
         })
-        .then(function (item) {
+        .then((item) => {
           expect(item.name).to.equal('Item2');
           return iterator.next();
         })
-        .then(function (item) {
+        .then((item) => {
           expect(item.name).to.equal('Item3');
           return iterator.next();
         })
-        .then(function (item) {
+        .then((item) => {
           expect(item.name).to.equal('Item4');
-          done();
         })
+        .then(done)
         .catch(done);
     });
-    it('should process all items sequentially when calling forEachAsync()', function (done) {
-      var iterator = this.Item.iterate();
-      var count = 1;
-      var fn = function(task, callback) {
-        setTimeout(function() {
-          expect(task.item.name).to.equal('Item'+count);
+    it('should process all items sequentially when calling forEachAsync()', (t, done) => {
+      const iterator = t.Item.iterate();
+      let count = 1;
+
+      iterator.forEachAsync((task, callback) => {
+        setTimeout(() => {
+          expect(task.item.name).to.equal(`Item${ count }`);
           count++;
           callback();
         }, 10);
-      }
-      iterator.forEachAsync(fn)
-        .then(function () {
-          done();
-        })
+      })
+        .then(done)
         .catch(done);
     });
   });
-
 });
